4a39b1dd5fe39c462f2ed16e5d5f3b1c16de73e5
jmk445 <jmk445123456@gmail.com>
2023-02-13 23:11:56 +0900

    초기 커밋.

---
+++ this_is_coding_test/greedy-1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int N;
+int main() {
+	cin >> N;
+	int coins[] = { 500, 100, 50, 10 };
+	int count = 0;
+	for (int i = 0; i < 4; i++) {
+		count += N / coins[i];
+		N = N % coins[i];
+	}
+
+	cout << count;
+}
